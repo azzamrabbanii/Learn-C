122966bcdb0df3dbda690b8a21c2a79f22483050
Muhammad Azzam <MazzamR08@gmail.com>
2024-10-01 10:01:03 +0700

    initial commit program 3

---
+++ Latihan/Program3.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MyApplication{
+
+    class Program2{
+        static void Main(string[] args)
+        {   
+            Console.Write("Masukan angka : ");
+            int i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.Write("Entered Number is an Even Number");
+                //Console.Read();
+            }
+            else
+            {
+                Console.Write("Entered Number is an Odd Number");
+                //Console.Read();
+            }
+
+        }
+    }
+}
