35c792e8d0b06b7830149246e37f19b102d4bdd7
Muhammad Azzam <MazzamR08@gmail.com>
2024-10-16 09:15:00 +0700

    update

--- Latihan/Program6.cs
+++ Latihan/Program6.cs
@@ -40,22 +40,31 @@ namespace MyApplication
             //     Console.WriteLine($"{number} adalah bilangan ganjil.");
             // }
 
-            int[] bilangan = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // int[] bilangan = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            for (int i = 0; i < bilangan.Length; i++)
+            // int totalGenap = 0;
+            // int totalGanjil = 0;
+
+            // for (int i = 0; i < bilangan.Length; i++)
                 
-            {
-                if (i % 2 == 0)
-                {   
-                    Console.WriteLine(bilangan[i] + " adalah bilangan genap");
-                }
-                else
-                {
-                    Console.WriteLine(bilangan[i] + " adalah bilangan ganjil");
+            // {
+            //     if (i % 2 == 0)
+            //     {   
+            //         Console.WriteLine(bilangan[i] + " adalah bilangan genap");
+            //         totalGanjil++;
+            //     }
+            //     else 
+            //     {
+            //         Console.WriteLine(bilangan[i] + " adalah bilangan ganjil");
+            //         totalGenap++;
+            //     }
+            // }
+            // Console.WriteLine();
+            // Console.WriteLine($"Total bilangan genap: {totalGenap}");
+            // Console.WriteLine($"Total bilangan ganjil: {totalGanjil}");
 
-                }
-                Thread.Sleep(500);
-            }
+            
+
         }
     }
 }
