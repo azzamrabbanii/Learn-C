df003497bb0a0b90bcd3a84d2ae0a40b664ccd24
Muhammad Azzam <MazzamR08@gmail.com>
2024-10-16 08:41:09 +0700

    Update new code

--- Latihan/Program6.cs
+++ Latihan/Program6.cs
@@ -1,22 +1,61 @@
-using System;
-
 namespace MyApplication
 {
-    class Program
+    class Program6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Mulai Program...");
-            int i = 10;
-            while(i > 0){
-                Console.WriteLine(i);
-                i--;
-                if(i == 0){
-                    break;
+            // Console.WriteLine("Mulai Program");
+            // int i = 10;
+            // while (i > 0)
+            // {
+            //     Console.Write(i); // Print the number
+
+            //     // Print dots beside the number
+            //     for (int j = 0; j < i; j++)
+            //     {
+            //         Console.Write("."); // Dots will be printed right beside the number
+            //     }
+
+            //     Console.WriteLine();
+
+            //     i--;
+            //     if (i == 0)
+            //     {
+            //         break;
+            //     }
+            //     Thread.Sleep(500);
+            // }
+            // Console.WriteLine("Selesai Program");
+
+            // Console.Write("Masukkan sebuah angka: ");
+            // int number = Convert.ToInt32(Console.ReadLine());
+
+            // // Mengecek apakah bilangan genap atau ganjil
+            // if (number % 2 == 0)
+            // {
+            //     Console.WriteLine($"{number} adalah bilangan genap.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{number} adalah bilangan ganjil.");
+            // }
+
+            int[] bilangan = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < bilangan.Length; i++)
+                
+            {
+                if (i % 2 == 0)
+                {   
+                    Console.WriteLine(bilangan[i] + " adalah bilangan genap");
+                }
+                else
+                {
+                    Console.WriteLine(bilangan[i] + " adalah bilangan ganjil");
+
                 }
                 Thread.Sleep(500);
             }
-            Console.WriteLine("Selesai Program...");
         }
     }
 }
