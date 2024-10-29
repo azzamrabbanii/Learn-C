4ebc93dcd1a62fd37ff22952a8e98d4116c3e581
Muhammad Azzam <MazzamR08@gmail.com>
2024-10-29 10:57:47 +0700

    update lagi

--- Latihan/Program6.cs
+++ Latihan/Program6.cs
@@ -1,119 +1,150 @@
-// namespace MyApplication
-// {
-//     class Program6
-//     {
-//         static void Main(string[] args)
-//         {
-//             // Console.WriteLine("Mulai Program");
-//             // int i = 10;
-//             // while (i > 0)
-//             // {
-//             //     Console.Write(i); // Print the number
+namespace MyApplication
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Mulai Program");
+            // int i = 10;
+            // while (i > 0)
+            // {
+            //     Console.Write(i); // Print the number
 
-//             //     // Print dots beside the number
-//             //     for (int j = 0; j < i; j++)
-//             //     {
-//             //         Console.Write("."); // Dots will be printed right beside the number
-//             //     }
+            //     // Print dots beside the number
+            //     for (int j = 0; j < i; j++)
+            //     {
+            //         Console.Write("."); // Dots will be printed right beside the number
+            //     }
 
-//             //     Console.WriteLine();
+            //     Console.WriteLine();
 
-//             //     i--;
-//             //     if (i == 0)
-//             //     {
-//             //         break;
-//             //     }
-//             //     Thread.Sleep(500);
-//             // }
-//             // Console.WriteLine("Selesai Program");
+            //     i--;
+            //     if (i == 0)
+            //     {
+            //         break;
+            //     }
+            //     Thread.Sleep(500);
+            // }
+            // Console.WriteLine("Selesai Program");
 
-//             // Console.Write("Masukkan sebuah angka: ");
-//             // int number = Convert.ToInt32(Console.ReadLine());
+            // Console.Write("Masukkan sebuah angka: ");
+            // int number = Convert.ToInt32(Console.ReadLine());
 
-//             // // Mengecek apakah bilangan genap atau ganjil
-//             // if (number % 2 == 0)
-//             // {
-//             //     Console.WriteLine($"{number} adalah bilangan genap.");
-//             // }
-//             // else
-//             // {
-//             //     Console.WriteLine($"{number} adalah bilangan ganjil.");
-//             // }
+            // // Mengecek apakah bilangan genap atau ganjil
+            // if (number % 2 == 0)
+            // {
+            //     Console.WriteLine($"{number} adalah bilangan genap.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{number} adalah bilangan ganjil.");
+            // }
 
-//             // int[] bilangan = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // int[] bilangan = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-//             // int totalGenap = 0;
-//             // int totalGanjil = 0;
+            // int totalGenap = 0;
+            // int totalGanjil = 0;
 
-//             // for (int i = 0; i < bilangan.Length; i++)
+            // for (int i = 0; i < bilangan.Length; i++)
 
-//             // {
-//             //     if (i % 2 == 0)
-//             //     {   
-//             //         Console.WriteLine(bilangan[i] + " adalah bilangan genap");
-//             //         totalGanjil++;
-//             //     }
-//             //     else 
-//             //     {
-//             //         Console.WriteLine(bilangan[i] + " adalah bilangan ganjil");
-//             //         totalGenap++;
-//             //     }
-//             // }
-//             // Console.WriteLine();
-//             // Console.WriteLine($"Total bilangan genap: {totalGenap}");
-//             // Console.WriteLine($"Total bilangan ganjil: {totalGanjil}");
+            // {
+            //     if (i % 2 == 0)
+            //     {   
+            //         Console.WriteLine(bilangan[i] + " adalah bilangan genap");
+            //         totalGanjil++;
+            //     }
+            //     else 
+            //     {
+            //         Console.WriteLine(bilangan[i] + " adalah bilangan ganjil");
+            //         totalGenap++;
+            //     }
+            // }
+            // Console.WriteLine();
+            // Console.WriteLine($"Total bilangan genap: {totalGenap}");
+            // Console.WriteLine($"Total bilangan ganjil: {totalGanjil}");
 
-//             // int[] angka = {-1,-23,-45,1,23,35,52};
-//             // int terbesar = angka[0];
+            // int[] angka = {-1,-23,-45,1,23,35,52};
+            // int terbesar = angka[0];
 
-//             // for (int i = 0; i < angka.Length;  i++){
-//             //     if(angka[i] < 0) {
-//             //         continue;
-//             //     }
+            // for (int i = 0; i < angka.Length;  i++){
+            //     if(angka[i] < 0) {
+            //         continue;
+            //     }
 
-//             //     if(angka[i] > terbesar) {
-//             //         terbesar = angka[i];
-//             //     }
+            //     if(angka[i] > terbesar) {
+            //         terbesar = angka[i];
+            //     }
 
-//             // }
-//             // Console.WriteLine($"Bilangan terbesar dalam array adalah: {terbesar}");
+            // }
+            // Console.WriteLine($"Bilangan terbesar dalam array adalah: {terbesar}");
 
-//             int[] numbers = { -10, -5, -7, -100};
-//             int? largest = null;
+            // int[] numbers = { -10, -5, -7, -100};
+            // int? largest = null;
 
-//             for (int i = 0; i < numbers.Length; i++)
-//             {
-//                 // Melewatkan bilangan negatif
-//                 if (numbers[i] < 0)
-//                 {
-//                     continue;
-//                 }
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     // Melewatkan bilangan negatif
+            //     if (numbers[i] < 0)
+            //     {
+            //         continue;
+            //     }
 
-//                 // Jika bilangan adalah kelipatan 5, hentikan pencarian dan kembalikan bilangan tersebut
-//                 if (numbers[i] % 5 == 0)
-//                 {
-//                     largest = numbers[i];
-//                     break; // Menghentikan pencarian karena menemukan kelipatan 5
-//                 }
+            //     // Jika bilangan adalah kelipatan 5, hentikan pencarian dan kembalikan bilangan tersebut
+            //     if (numbers[i] % 5 == 0)
+            //     {
+            //         largest = numbers[i];
+            //         break; // Menghentikan pencarian karena menemukan kelipatan 5
+            //     }
 
-//                 // Menentukan bilangan terbesar
-//                 if (largest == null || numbers[i] > largest)
-//                 {
-//                     largest = numbers[i];
-//                 }
-//             }
+            //     // Menentukan bilangan terbesar
+            //     if (largest == null || numbers[i] > largest)
+            //     {
+            //         largest = numbers[i];
+            //     }
+            // }
 
-//             // Jika largest masih null, berarti semua bilangan negatif atau array kosong
-//             if (largest == null)
-//             {
-//                 Console.WriteLine("Semua bilangan negatif atau array kosong.");
-//             }
-//             else
-//             {
-//                 Console.WriteLine($"Bilangan terbesar adalah: {largest}");
-//             }
+            // // Jika largest masih null, berarti semua bilangan negatif atau array kosong
+            // if (largest == null)
+            // {
+            //     Console.WriteLine("Semua bilangan negatif atau array kosong.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"Bilangan terbesar adalah: {largest}");
+            // }
 
-//         }
-//     }
-// }
+            // for (int i = 5; i > 0; i--)
+            // {
+            //     for (int j = 0; j < 6 - i; j++)
+            //     {
+            //         Console.Write(".");
+            //     }
+            //     Console.WriteLine();
+            //     Thread.Sleep(500);
+            // }
+            // int n = 10; //number of rows
+
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Thread.Sleep(250);
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                Thread.Sleep(250);
+            }
+        }
+    }
+}
 
